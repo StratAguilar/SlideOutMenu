e294b808f3d1e2d9e3c880d1217c53e15411e4ad
Strat Aguilar <strataguilar@gmail.com>
2016-09-22 11:23:55 -0700

    initial commit

---
+++ SlideOutMenu/SlideOutContainerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SlideOutContainerViewController.swift
+//  SlideOutMenu
+//
+//  Created by Strat Aguilar on 9/22/16.
+//  Copyright © 2016 Strat Aguilar. All rights reserved.
+//
+
+import UIKit
+
+class SlideOutContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SlideOutMenuViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SlideOutMenuViewController.swift
+//  SlideOutMenu
+//
+//  Created by Strat Aguilar on 9/22/16.
+//  Copyright © 2016 Strat Aguilar. All rights reserved.
+//
+
+import UIKit
+
+protocol SlideOutMenuViewControllerDelegate: class{
+  func closeSlideOutMenu()
+}
+
+class SlideOutMenuViewController: UIViewController {
+
+    weak var delgate: SlideOutMenuViewControllerDelegate?
+  
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.layer.cornerRadius = 10
+        self.view.clipsToBounds = true
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
